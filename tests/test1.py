540a0846e201e51110c0a56f8ac0e862a2e02ce5
Scott Belliveau <s1belliveau.student.bridgew.edu>
2022-02-09 11:56:47 -0500

    corrected integration test 1 to be more ideal

--- tests/test1.py
+++ tests/test1.py
@@ -3,7 +3,6 @@ import sqlite3
 from typing import Tuple
 
 
-
 def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
     db_connection = sqlite3.connect(filename)
     cursor = db_connection.cursor()
