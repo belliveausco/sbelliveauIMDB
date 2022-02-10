2a86ab378b8a5b4f3feaba7627dd3fd27964bce9
Scott Belliveau <s1belliveau.student.bridgew.edu>
2022-02-10 18:51:58 -0500

    updated tests

--- tests/test1.py
+++ tests/test1.py
@@ -3,21 +3,19 @@ import sqlite3
 from typing import Tuple
 
 
-def test_open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
-    db_connection = sqlite3.connect(filename)
-    cursor = db_connection.cursor()
-    return db_connection, cursor
+def test_main():
+    def test_open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+        db_connection = sqlite3.connect(filename)
+        cursor = db_connection.cursor()
+        return db_connection, cursor
+
+    def test_data_size(cursor: sqlite3.Cursor):
+        final_result = imdb_setup_database.test_size(cursor)
+        assert final_result == 251
+
+    database = 'imDb.db'
+    conn, curs = test_open_db(database)
+    test_data_size(curs)
 
 
-def test_close_db(connection: sqlite3.Connection):
-    connection.close()
-
-
-def test_data_size(cursor: sqlite3.Cursor):
-    final_result = imdb_setup_database.test_size(cursor)
-    assert final_result == 251
-
-
-database = 'imDb.db'
-conn, curs = test_open_db(database)
-test_data_size(curs)
+test_main()
--- tests/test2.py
+++ tests/test2.py
@@ -4,66 +4,66 @@ import sqlite3
 from typing import Tuple
 
 
-def test_open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
-    db_connection = sqlite3.connect(filename)
-    cursor = db_connection.cursor()
-    return db_connection, cursor
+def test_main():
+    def test_open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+        db_connection = sqlite3.connect(filename)
+        cursor = db_connection.cursor()
+        return db_connection, cursor
+
+    def test_close_db(connection: sqlite3.Connection):
+        connection.close()
+
+    def test_show_dictionary(cursor: sqlite3.Cursor, conn: sqlite3.Connection):
+        cursor.execute("""DROP TABLE IF EXISTS test_top250_tv_shows""")
+        cursor.execute("""CREATE TABLE IF NOT EXISTS test_top250_tv_shows(id TEXT PRIMARY KEY,
+                                    title TEXT NOT NULL,
+                                    fullTitle TEXT NOT NULL,
+                                    year TEXT NOT NULL,
+                                    crew TEXT NOT NULL,
+                                    imDbRating TEXT NOT NULL,
+                                    imDbRatingCount TEXT NOT NULL);""")
+        conn.commit()
+
+        data = {
+            "items": [{"id": "tt5555555", "rank": "300", "title": "Capstone", "fullTitle": "Capstone (2020)",
+                       "year": "2020", "crew": "Miserable Students", "imDbRating": "8.5",
+                       "imDbRatingCount": "64"}]}
+
+        cursor.execute("""INSERT INTO test_top250_tv_shows (id, title, fullTitle, year, crew, imDbRating, imDbRatingCount)
+                                  VALUES (?, ?, ?, ?, ?, ?, ?)""", (data.get("items")[0].get("id"),
+                                                                    data.get("items")[0].get("title"),
+                                                                    data.get("items")[0].get("fullTitle"),
+                                                                    data.get("items")[0].get("year"),
+                                                                    data.get("items")[0].get("crew"),
+                                                                    data.get("items")[0].get("imDbRating"),
+                                                                    data.get("items")[0].get("imDbRatingCount")
+                                                                    ))
+        conn.commit()
+
+        test_close_db(conn)
+        conn, cursor = test_open_db('test_250.db')
+
+        result = cursor.execute(f'SELECT * FROM test_top250_tv_shows')
+        for row in result:
+            print(f'id: {row[0]}\ntitle: {row[1]}\nfullTitle: {row[2]}\nyear: {row[3]}\ncrew: {row[4]}\n'
+                  f'imDbRating: {row[5]}\nimDbRatingCount: {row[6]}\n')
+
+        cursor.execute('''SELECT * FROM test_top250_tv_shows''')
+        results = cursor.fetchall()
+        print(len(results))
+        assert len(results) == 1
+        cursor.execute('''SELECT id FROM test_top250_tv_shows''')
+        id_verification = cursor.fetchall()
+        test_id = id_verification[0]
+        print(test_id[0])
+        assert test_id[0] == 'tt5555555'
+
+    def main():
+        database = 'test_250.db'
+        conn, curs = test_open_db(database)
+        test_show_dictionary(curs, conn)
+
+    main()
 
 
-def test_close_db(connection: sqlite3.Connection):
-    connection.close()
-
-
-def test_show_dictionary(cursor: sqlite3.Cursor, conn: sqlite3.Connection):
-    cursor.execute("""DROP TABLE IF EXISTS test_top250_tv_shows""")
-    cursor.execute("""CREATE TABLE IF NOT EXISTS test_top250_tv_shows(id TEXT PRIMARY KEY,
-                                title TEXT NOT NULL,
-                                fullTitle TEXT NOT NULL,
-                                year TEXT NOT NULL,
-                                crew TEXT NOT NULL,
-                                imDbRating TEXT NOT NULL,
-                                imDbRatingCount TEXT NOT NULL);""")
-    conn.commit()
-
-    data = {
-        "items": [{"id": "tt5555555", "rank": "300", "title": "Capstone", "fullTitle": "Capstone (2020)",
-                   "year": "2020", "crew": "Miserable Students", "imDbRating": "8.5",
-                   "imDbRatingCount": "64"}]}
-
-    cursor.execute("""INSERT INTO test_top250_tv_shows (id, title, fullTitle, year, crew, imDbRating, imDbRatingCount)
-                              VALUES (?, ?, ?, ?, ?, ?, ?)""", (data.get("items")[0].get("id"),
-                                                                data.get("items")[0].get("title"),
-                                                                data.get("items")[0].get("fullTitle"),
-                                                                data.get("items")[0].get("year"),
-                                                                data.get("items")[0].get("crew"),
-                                                                data.get("items")[0].get("imDbRating"),
-                                                                data.get("items")[0].get("imDbRatingCount")
-                                                                ))
-    conn.commit()
-
-    test_close_db(conn)
-    conn, cursor = test_open_db('test_250.db')
-
-    result = cursor.execute(f'SELECT * FROM test_top250_tv_shows')
-    for row in result:
-        print(f'id: {row[0]}\ntitle: {row[1]}\nfullTitle: {row[2]}\nyear: {row[3]}\ncrew: {row[4]}\n'
-              f'imDbRating: {row[5]}\nimDbRatingCount: {row[6]}\n')
-
-    cursor.execute('''SELECT * FROM test_top250_tv_shows''')
-    results = cursor.fetchall()
-    print(len(results))
-    assert len(results) == 1
-    cursor.execute('''SELECT id FROM test_top250_tv_shows''')
-    id_verification = cursor.fetchall()
-    test_id = id_verification[0]
-    print(test_id[0])
-    assert test_id[0] == 'tt5555555'
-
-
-def main():
-    database = 'test_250.db'
-    conn, curs = test_open_db(database)
-    test_show_dictionary(curs, conn)
-
-
-main()
+test_main()
