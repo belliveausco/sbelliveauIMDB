63665b09da9e1830d730b3bfe99a92ece5b6cd42
Scott Belliveau <s1belliveau.student.bridgew.edu>
2022-02-10 17:46:26 -0500

    updated ids for queries could not implement note could not use 200 place rankings so used 201 place instead 200 (impractical jokers has no rankings)

--- imdb_setup_database.py
+++ imdb_setup_database.py
@@ -146,7 +146,7 @@ def populate_no_1_show(cursor: sqlite3.Cursor, conn: sqlite3.Connection):
 
 
 def populate_no_50_show(cursor: sqlite3.Cursor, conn: sqlite3.Connection):
-    loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/tt2297757"
+    loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/tt0081834"
     results = requests.get(loc)
     if results.status_code != 200:
         print("help!")
@@ -226,7 +226,7 @@ def populate_no_100_show(cursor: sqlite3.Cursor, conn: sqlite3.Connection):
 
 
 def populate_no_200_show(cursor: sqlite3.Cursor, conn: sqlite3.Connection):
-    loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/tt7472896"
+    loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/tt1492966"
     results = requests.get(loc)
     if results.status_code != 200:
         print("help!")
