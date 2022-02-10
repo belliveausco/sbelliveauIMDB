f35aad57a8dca97941c7fa2cada419351227d517
Scott Belliveau <s1belliveau.student.bridgew.edu>
2022-02-10 14:16:44 -0500

    changed index error in populate_250, needs to be 250

--- imdb_setup_database.py
+++ imdb_setup_database.py
@@ -65,7 +65,7 @@ def populate_top250_tv_shows(cursor: sqlite3.Cursor, conn: sqlite3.Connection):
         print("help!")
         return
     data = results.json()
-    for i in range(0, 249):
+    for i in range(0, 250):
         cursor.execute("""INSERT INTO top250_tv_shows (id, title, fullTitle, year, crew, imDbRating, imDbRatingCount)
                        VALUES (?, ?, ?, ?, ?, ?, ?)""", (data.get("items")[i].get("id"),
                                                          data.get("items")[i].get("title"),
