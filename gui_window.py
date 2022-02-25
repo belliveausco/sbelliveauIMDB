b5b832c12fcf9daf51391bd9ad7c48d5b054d06e
Scott Belliveau <s1belliveau.student.bridgew.edu>
2022-02-25 12:46:16 -0500

    Updated main to run gui application added update data method and separate window for data visualization method

---
+++ gui_window.py
@@ -0,0 +1,89 @@
+import sqlite3
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLineEdit, QLabel, QRadioButton, QMessageBox
+import pandas as pd
+import sqlite3 as sql
+import imdb_setup_database
+import sys
+
+
+class Window(QMainWindow):
+    def __init__(self, conn: sqlite3.Connection, curs: sqlite3.Cursor):
+        super().__init__()
+        # Database tools
+        self.cursor = curs
+        self.connection = conn
+
+        # GUI Buttons
+        self.update_the_data = QPushButton(self)
+        self.run_the_data_visualization = QPushButton(self)
+
+        # Window
+        self.window = Visualization_Methods(self.connection, self.cursor)
+        self.window.hide()
+
+        # Setting up UI
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.setWindowTitle("imDb Database")
+        self.setGeometry(50, 50, 800, 800)
+
+        # Button Area
+        self.update_the_data.setText("Update The Data")
+        self.update_the_data.move(200, 360)
+        self.update_the_data.resize(180, 40)
+        self.update_the_data.clicked.connect(self.update_the_data_clicked)
+
+        self.run_the_data_visualization.setText("Run The Data Visualization")
+        self.run_the_data_visualization.move(400, 360)
+        self.run_the_data_visualization.resize(180, 40)
+        self.run_the_data_visualization.clicked.connect(self.run_the_data_visualization_clicked)
+
+        # Showing Ui
+        self.show()
+
+    @staticmethod
+    def update_the_data_clicked():
+        conn = sql.connect('imDb.db')
+        cursor = conn.cursor()
+        imdb_setup_database.setup_top250_tv_shows(cursor, conn)
+        imdb_setup_database.setup_top250_tv_shows(cursor, conn)
+        imdb_setup_database.setup_user_ratings(cursor, conn)
+        imdb_setup_database.populate_top250_tv_shows(cursor, conn)
+        imdb_setup_database.add_wot_top250_tv_shows(cursor, conn)
+        imdb_setup_database.populate_no_1_show(cursor, conn)
+        imdb_setup_database.populate_no_50_show(cursor, conn)
+        imdb_setup_database.populate_no_100_show(cursor, conn)
+        imdb_setup_database.populate_no_200_show(cursor, conn)
+        imdb_setup_database.populate_wot_show(cursor, conn)
+        imdb_setup_database.setup_top250_movies(cursor, conn)
+        imdb_setup_database.populate_top250_movies(cursor, conn)
+        imdb_setup_database.setup_most_popular_movies(cursor, conn)
+        imdb_setup_database.setup_movie_user_ratings(cursor, conn)
+        imdb_setup_database.populate_most_popular_movies(cursor, conn)
+        imdb_setup_database.populate_rankings_movie(cursor, conn)
+        imdb_setup_database.setup_most_popular_tv_shows(cursor, conn)
+        imdb_setup_database.populate_most_popular_tv_shows(cursor, conn)
+
+    def run_the_data_visualization_clicked(self):
+        self.window.show()
+
+
+class Visualization_Methods(QMainWindow):
+    def __init__(self, conn: sqlite3.Connection, curs: sqlite3.Cursor):
+        super().__init__()
+
+        # Database tools
+        self.cursor = curs
+        self.connection = conn
+
+        # Setting up UI
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.setWindowTitle("Data Visualization")
+        self.setGeometry(50, 50, 800, 800)
+
+        # Showing Ui
+        self.show()
--- main.py
+++ main.py
@@ -1,137 +1,16 @@
-import requests
-import secrets
+import sys
+import sqlite3 as sql
+import gui_window
 
 
-# Scott Belliveau
-
-def no_1_show():
-    a = open("data.txt", "a+")
-    a.write(f"Ratings for No 1 TV Show")
-    a.write(f"\n")
-    loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/tt5491994"  # look up tt number code for shows you are looking at
-    results = requests.get(loc)
-    if results.status_code != 200:
-        print("help!")
-        return
-    data = results.json()
-    for key, value in data.items():
-        if key != 'ratings':
-            a.write(f"{key, value}""\n")
-        elif key == 'ratings':
-            ratings_list = list(value)
-            for x in ratings_list:
-                a.write(f"{x}""\n")
-    a.write(f"\n")
-    a.close()
+def main():
+    # Main database stuff here
+    conn = sql.connect('imDb.db')
+    cursor = conn.cursor()
+    app = gui_window.QApplication(sys.argv)
+    ex = gui_window.Window(conn, cursor)
+    ex.isHidden()
+    sys.exit(app.exec_())
 
 
-def no_50_show():
-    a = open("data.txt", "a+")
-    a.write(f"Ratings for No 50 TV Show")
-    a.write(f"\n")
-    loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/tt0081834"  # look up tt number code for shows you are looking at
-    results = requests.get(loc)
-    if results.status_code != 200:
-        print("help!")
-        return
-    data = results.json()
-    for key, value in data.items():
-        if key != 'ratings':
-            a.write(f"{key, value}""\n")
-        elif key == 'ratings':
-            ratings_list = list(value)
-            for x in ratings_list:
-                a.write(f"{x}""\n")
-    a.write(f"\n")
-    a.close()
-
-
-def no_100_show():
-    a = open("data.txt", "a+")
-    a.write(f"Ratings for No 100 TV Show")
-    a.write(f"\n")
-    loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/tt0096697"  # look up tt number code for shows you are looking at
-    results = requests.get(loc)
-    if results.status_code != 200:
-        print("help!")
-        return
-    data = results.json()
-    for key, value in data.items():
-        if key != 'ratings':
-            a.write(f"{key, value}""\n")
-        elif key == 'ratings':
-            ratings_list = list(value)
-            for x in ratings_list:
-                a.write(f"{x}""\n")
-    a.write(f"\n")
-    a.close()
-
-
-def no_200_show():
-    a = open("data.txt", "a+")
-    a.write(f"Ratings for No 200 TV Show")
-    a.write(f"\n")
-    loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/tt1492966"  # look up tt number code for shows you are looking at
-    results = requests.get(loc)
-    if results.status_code != 200:
-        print("help!")
-        return
-    data = results.json()
-    for key, value in data.items():
-        if key != 'ratings':
-            a.write(f"{key, value}""\n")
-        elif key == 'ratings':
-            ratings_list = list(value)
-            for x in ratings_list:
-                a.write(f"{x}""\n")
-    a.write(f"\n")
-    a.close()
-
-
-def the_wheel_of_time():
-    a = open("data.txt", "a+")
-    a.write(f"Ratings for The Wheel of Time TV Show")
-    a.write(f"\n")
-    loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/tt7462410"  # look up tt number code for shows you are looking at
-    results = requests.get(loc)
-    if results.status_code != 200:
-        print("help!")
-        return
-    data = results.json()
-    for key, value in data.items():
-        if key != 'ratings':
-            a.write(f"{key, value}""\n")
-        elif key == 'ratings':
-            ratings_list = list(value)
-            for x in ratings_list:
-                a.write(f"Ratings")
-                a.write(f"{x}""\n")
-    a.write(f"\n")
-    a.close()
-
-
-def top_250_shows():
-    a = open("data.txt", "a+")
-    a.write(f"Top 250 TV Shows""\n\n""")
-    loc = f"https://imdb-api.com/en/API/Top250TVs/{secrets.secret_key}"
-    results = requests.get(loc)
-    if results.status_code != 200:
-        print("help!")
-        return
-    data = results.json()
-    top_250_list = data["items"]
-    for i in range(0, 250):
-        top_250_dictionary = top_250_list[i]
-        for key, value in top_250_dictionary.items():
-            a.write(f"{(key, str(value))}""\n\n")
-    a.close()
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    no_1_show()
-    no_50_show()
-    no_100_show()
-    no_200_show()
-    the_wheel_of_time()
-    top_250_shows()
+main()
