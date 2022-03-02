f31aa9e8ab6bc8fe66e49674dd9f4ab603deff05
Scott Belliveau <s1belliveau.student.bridgew.edu>
2022-03-02 18:19:11 -0500

    added the_most_popular_movies_by_rankUpDown.py organizes data by top rankUpDowns

--- gui_window.py
+++ gui_window.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
 import sqlite3 as sql
 import imdb_setup_database
 import the_most_popular_movies_by_ranking
+import the_most_popular_movies_by_rankUpDown
 
 
 class Window(QMainWindow):
@@ -78,33 +79,34 @@ class Visualization_Methods(QMainWindow):
         # Function windows
         self.window = the_most_popular_movies_by_ranking.App(self.connection, self.cursor)
         self.window.hide()
-        '''
-        self.window1 = most_popular_movies_by_rankUpDown(self.connection, self.cursor)
-        self.window2 = most_popular_tv_by_rank(self.connection, self.cursor)
-        self.window3 = most_popular_tv_by_rankUpDown(self.connection, self.cursor)
-        self.window4 = pop_movie_rankings(self.connection, self.cursor)
-        self.window5 = top_250_tv_show_rankings(self.connection, self.cursor)
-        self.window6 = graph_most_popular_movies_moving_up(self.connection, self.cursor)
-        self.window7 = graph_most_popular_movies_moving_down(self.connection, self.cursor)
-        self.window8 = graph_most_popular_tv_moving_up(self.connection, self.cursor)
-        self.window9 = graph_most_popular_tv_moving_down(self.connection, self.cursor)
-        self.window10 = top_250_movies_and_most_popular(self.connection, self.cursor)
-        self.window11 = top_250_tv_and_most_popular(self.connection, self.cursor)
+        self.window1 = the_most_popular_movies_by_rankUpDown.App(self.connection, self.cursor)
         self.window1.hide()
+        '''
+        self.window2 = the_most_popular_tv_by_rank(self.connection, self.cursor)
         self.window2.hide()
+        self.window3 = the_most_popular_tv_by_rankUpDown(self.connection, self.cursor)
         self.window3.hide()
+        self.window4 = pop_movie_rankings(self.connection, self.cursor)
         self.window4.hide()
+        self.window5 = top_250_tv_show_rankings(self.connection, self.cursor)
         self.window5.hide()
+        self.window6 = graph_most_popular_movies_moving_up(self.connection, self.cursor)
         self.window6.hide()
+        self.window7 = graph_most_popular_movies_moving_down(self.connection, self.cursor)
         self.window7.hide()
+        self.window8 = graph_most_popular_tv_moving_up(self.connection, self.cursor)
         self.window8.hide()
+        self.window9 = graph_most_popular_tv_moving_down(self.connection, self.cursor)
         self.window9.hide()
+        self.window10 = top_250_movies_and_most_popular(self.connection, self.cursor)
         self.window10.hide()
+        self.window11 = top_250_tv_and_most_popular(self.connection, self.cursor)
         self.window11.hide()
         '''
 
         # Menu Buttons
         self.test_button = QPushButton(self)
+        self.test_button1 = QPushButton(self)
 
         # Setting up UI
         self.setup_ui()
@@ -118,8 +120,16 @@ class Visualization_Methods(QMainWindow):
         self.test_button.setText('most popular movies sorted by ranking')
         self.test_button.clicked.connect(self.test_button_connection)
 
+        self.test_button1.resize(500, 30)
+        self.test_button1.move(150, 100)
+        self.test_button1.setText('most popular movies sorted by rankUpDown')
+        self.test_button1.clicked.connect(self.test_button1_connection)
+
         # Showing Ui
         self.show()
 
     def test_button_connection(self):
         self.window.show()
+
+    def test_button1_connection(self):
+        self.window1.show()
---
+++ the_most_popular_movies_by_rankUpDown.py
@@ -0,0 +1,72 @@
+import sqlite3
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QTableWidget, QTableWidgetItem, QVBoxLayout
+import sqlite3 as sql
+
+
+class App(QWidget):
+
+    def __init__(self, conn: sqlite3.Connection, curs: sqlite3.Cursor):
+        super().__init__()
+        # Database tools
+        self.cursor = curs
+        self.connection = conn
+
+        self.title = 'Most Popular Movies By Rankings'
+        self.left = 0
+        self.top = 0
+        self.width = 1250
+        self.height = 800
+
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.createTable()
+
+        # Add box layout, add table to box layout and add box layout to widget
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.tableWidget)
+        self.setLayout(self.layout)
+
+        # Show widget
+        self.show()
+
+    def createTable(self):
+        # Create table
+        self.tableWidget = QTableWidget(self)
+        self.tableWidget.setRowCount(100)  # rows
+        self.tableWidget.setColumnCount(9)  # Columns
+        self.tableWidget.setHorizontalHeaderLabels(["pop_movie_id", "rank", "rankUpDown", "title", "fullTitle", "year",
+                                                    "crew", "imDbRating", "imDbRatingCount"])
+        conn = sql.connect('imDb.db')
+        cursor = conn.cursor()
+        query = "SELECT * FROM most_popular_movies ORDER BY rankUpDown DESC;"
+
+        table_row = 0
+        for row in cursor.execute(query):
+            self.tableWidget.setItem(table_row, 0, QTableWidgetItem(row[0]))
+            self.tableWidget.setItem(table_row, 1, QTableWidgetItem(str(row[1])))
+            self.tableWidget.setItem(table_row, 2, QTableWidgetItem(str(row[2])))
+            self.tableWidget.setItem(table_row, 3, QTableWidgetItem(row[3]))
+            self.tableWidget.setItem(table_row, 4, QTableWidgetItem(row[4]))
+            self.tableWidget.setItem(table_row, 5, QTableWidgetItem(row[5]))
+            self.tableWidget.setItem(table_row, 6, QTableWidgetItem(row[6]))
+            self.tableWidget.setItem(table_row, 7, QTableWidgetItem(row[7]))
+            self.tableWidget.setItem(table_row, 8, QTableWidgetItem(row[8]))
+            table_row += 1
+
+        # Show widget
+        self.show()
+
+
+def main():
+    conn = sql.connect('imDb.db')
+    cursor = conn.cursor()
+    app = QApplication(sys.argv)
+    ex = App(conn, cursor)
+    ex.isHidden()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
